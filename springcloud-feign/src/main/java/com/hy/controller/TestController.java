054a70592e1ccf982d65a7dd35c177fa79aad119
wanghai <wangh_2@sina.com>
2019-07-31 13:51:40 +0800

    feign

---
+++ springcloud-feign/src/main/java/com/hy/controller/TestController.java
@@ -0,0 +1,27 @@
+package com.hy.controller;
+
+import com.hy.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: wanghai
+ * @Date:2019/7/31 13:42
+ * @Copyright:reach-life
+ * @Description:
+ */
+@RestController
+public class TestController {
+
+    @Autowired
+    private TestService testService;
+
+    @RequestMapping(value = "/testHello",method = RequestMethod.GET)
+    public String testHello(@RequestParam(value = "name") String name) {
+        return testService.testHello(name);
+    }
+
+}
---
+++ springcloud-feign/src/main/java/com/hy/service/TestService.java
@@ -0,0 +1,19 @@
+package com.hy.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @Author: wanghai
+ * @Date:2019/7/31 13:38
+ * @Copyright:reach-life
+ * @Description:
+ */
+@FeignClient(name = "service-client1")
+public interface TestService
+{
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String testHello(@RequestParam(name = "name") String name);
+}

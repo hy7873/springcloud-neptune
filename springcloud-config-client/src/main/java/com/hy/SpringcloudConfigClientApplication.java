728b1fd29f4d723c2f1a49ec53a7672168b2f607
wanghai <wangh_2@sina.com>
2019-08-05 11:06:26 +0800

    config

---
+++ springcloud-config-client/src/main/java/com/hy/SpringcloudConfigClientApplication.java
@@ -0,0 +1,25 @@
+package com.hy;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringcloudConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudConfigClientApplication.class, args);
+	}
+
+	@Value("${key1}")
+	private String key1;
+
+	@RequestMapping("/test-config")
+	public String test() {
+		return key1;
+	}
+
+}

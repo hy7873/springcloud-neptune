54e07853c738ae850d1649c658494b0b669b3d4f
wanghai <wangh_2@sina.com>
2019-08-05 17:27:50 +0800

    config

--- springcloud-eureka-client/src/main/java/com/hy/SpringcloudEurekaClientApplication.java
+++ springcloud-eureka-client/src/main/java/com/hy/SpringcloudEurekaClientApplication.java
@@ -2,6 +2,7 @@ package com.hy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
+@RefreshScope
 public class SpringcloudEurekaClientApplication {
 
 	public static void main(String[] args) {
